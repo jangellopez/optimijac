947c2a5d38cab16d9ad885682ad75a00e4ac19f2
diegoamb8 <79332482+diegoamb8@users.noreply.github.com>
2022-05-25 18:25:08 -0500

    Conexion Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:optimijac/screens/login.dart';
 
 import 'screens/menu.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDlaLNftby2f3oySIPTJo7OG5UoJCzpHvw",
+      appId: "1:827456482415:android:db772536ed2cefb181b19a",
+      messagingSenderId: "827456482415",
+      projectId: "optimijac",
+    ),
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,9 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Optimijac',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.rubik().fontFamily
-      ),
+      theme: ThemeData(fontFamily: GoogleFonts.rubik().fontFamily),
       home: Menu(),
     );
   }
